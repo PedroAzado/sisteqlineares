cd9e812e7c7ea3dde144de1926b38d6c6da0662f
PedroAzado <31451959+PedroAzado@users.noreply.github.com>
2017-09-11 20:36:22 -0300

    inicializa matriz

---
+++ mat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+#define L 4
+#define C 5
+
+main()
+{
+	float A[4][5] = {{1,1,0,3,4},{2,1,-1,1,1},{3,-1,-1,2,-3},{-1,2,3,-1,4}};
+	int i,j,k;
+	float lamba, aux;
+}
