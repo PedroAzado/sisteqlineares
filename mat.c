461f39e5c5cd54178508c3ac7428328c0c50504b
PedroAzado <31451959+PedroAzado@users.noreply.github.com>
2017-09-11 20:39:12 -0300

    imprime matriz

--- mat.c
+++ mat.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+void imprime(float A[L][C])
+{
+	int i, j;
+	
+	for(i=0;i<L;i++)
+	{
+		for(j=0;j<C;j++)
+			printf("%f\t", A[i][j]);
+		
+		printf("\n");
+	}
+}
+
 #define L 4
 #define C 5
 
@@ -9,4 +22,6 @@ main()
 	float A[4][5] = {{1,1,0,3,4},{2,1,-1,1,1},{3,-1,-1,2,-3},{-1,2,3,-1,4}};
 	int i,j,k;
 	float lamba, aux;
+
+	imprime(A);
 }
